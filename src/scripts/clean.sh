e582b005a5b85dac446c18d790b2d4440fbc884a
cuixu66 <cuixu66@hotmail.com>
2014-07-31 10:16:46 -0700

    fixed errors in the cleanup script

--- src/scripts/clean.sh
+++ src/scripts/clean.sh
@@ -1,16 +1,3 @@
 #!/bin/bash
 
-PROJECT_DIR='~/xcui/new_project/kvtest/src/'
-
-for i in h{1..4}
-do
-    ssh ${i} "kill -9 $(ps aux | grep server.p\[y\] | awk '{print $2}')"
-    echo server stopped at ${i}
-done
-
-
-for i in h{1..4}
-do
-    ssh ${i} "kill -9 $(ps aux | grep server.p\[y\] | awk '{print $2}')"
-    echo server stopped at ${i}
-done
+ps aux | grep server.py | awk '{print $2}' | xargs kill -9
