93e3eb15f79e087ab1f1a99c1ee87467565cfc68
cuixu66 <cuixu66@hotmail.com>
2014-08-07 16:32:17 -0400

    Modified scripts so queue_new.sh can take bandwidth capacity as input

--- src/scripts/queues_new.sh
+++ src/scripts/queues_new.sh
@@ -1,23 +1,31 @@
 #!/bin/bash
 
+
+if [ ${#} -ne 1 ]
+then
+    echo "Usage: ${0} bandwidth_in_bps"
+fi
+
+# total_bandwidth=1000000
+total_bandwidth=${1}
+
 # Add queues with varying minimum bandwidth reservations to each switch port
 # queue 0 traffic will have priority over all other traffic
-
 for switch in $(sudo ovs-vsctl list-br); do
     for port in $(sudo ovs-vsctl list-ports $switch); do
         sudo ovs-vsctl -- set Port $port qos=@newqos -- \
-          --id=@newqos create QoS type=linux-htb other-config:max-rate=1000000 \
+          --id=@newqos create QoS type=linux-htb other-config:max-rate=${total_bandwidth} \
           queues=0=@q0,1=@q1,2=@q2,3=@q3,4=@q4,5=@q5,6=@q6,7=@q7,8=@q8,9=@q9,10=@q10 -- \
           --id=@q0 create Queue other-config:priority=0 -- \
-          --id=@q1 create Queue other-config:min-rate=100000 other-config:priority=1 -- \
-          --id=@q2 create Queue other-config:min-rate=200000 other-config:priority=1 -- \
-          --id=@q3 create Queue other-config:min-rate=300000 other-config:priority=1 -- \
-          --id=@q4 create Queue other-config:min-rate=400000 other-config:priority=1 -- \
-          --id=@q5 create Queue other-config:min-rate=500000 other-config:priority=1 -- \
-          --id=@q6 create Queue other-config:min-rate=600000 other-config:priority=1 -- \
-          --id=@q7 create Queue other-config:min-rate=700000 other-config:priority=1 -- \
-          --id=@q8 create Queue other-config:min-rate=800000 other-config:priority=1 -- \
-          --id=@q9 create Queue other-config:min-rate=900000 other-config:priority=1 -- \
-          --id=@q10 create Queue other-config:min-rate=1000000 other-config:priority=1
+          --id=@q1 create Queue other-config:min-rate=$((total_bandwidth/10*1)) other-config:priority=1 -- \
+          --id=@q2 create Queue other-config:min-rate=$((total_bandwidth/10*2)) other-config:priority=1 -- \
+          --id=@q3 create Queue other-config:min-rate=$((total_bandwidth/10*3)) other-config:priority=1 -- \
+          --id=@q4 create Queue other-config:min-rate=$((total_bandwidth/10*4)) other-config:priority=1 -- \
+          --id=@q5 create Queue other-config:min-rate=$((total_bandwidth/10*5)) other-config:priority=1 -- \
+          --id=@q6 create Queue other-config:min-rate=$((total_bandwidth/10*6)) other-config:priority=1 -- \
+          --id=@q7 create Queue other-config:min-rate=$((total_bandwidth/10*7)) other-config:priority=1 -- \
+          --id=@q8 create Queue other-config:min-rate=$((total_bandwidth/10*8)) other-config:priority=1 -- \
+          --id=@q9 create Queue other-config:min-rate=$((total_bandwidth/10*9)) other-config:priority=1 -- \
+          --id=@q10 create Queue other-config:min-rate=$((total_bandwidth/10*10)) other-config:priority=1
     done
 done
--- src/scripts/test.sh
+++ src/scripts/test.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
+if [ ${#} -ne 1 ]
+then
+    echo "Usage: ${0} link_bandwidth_in_bsp"
+fi
+
+total_bandwidth=${1}
+
 PROJECT_DIR='/mininet/src/'
 
 mkdir -p ${PROJECT_DIR}/out
 rm -rf  ~/.ssh/known_hosts
 
-bash /mininet/src/scripts/queues_new.sh
+bash /mininet/src/scripts/queues_new.sh ${total_bandwidth}
 
 for i in 10.0.0.{1..4}
 do
